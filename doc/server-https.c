961c125d05342dee7076bdf1f16db2f12c34f814
shuoming zhang <zhangshuoming17@mails.ucas.ac.cn>
2021-11-16 00:11:44 +0800

    initialize

--- doc/server-https.c
+++ doc/server-https.c
@@ -13,28 +13,24 @@
 #include <event2/listener.h>
 #include <event2/bufferevent_ssl.h>
 
-static void
-ssl_readcb(struct bufferevent * bev, void * arg)
-{
+static void ssl_readcb(struct bufferevent *bev, void *arg) {
     struct evbuffer *in = bufferevent_get_input(bev);
 
     printf("Received %zu bytes\n", evbuffer_get_length(in));
     printf("----- data ----\n");
-    printf("%.*s\n", (int)evbuffer_get_length(in), evbuffer_pullup(in, -1));
+    printf("%.*s\n", (int) evbuffer_get_length(in), evbuffer_pullup(in, -1));
 
     bufferevent_write_buffer(bev, in);
 }
 
-static void
-ssl_acceptcb(struct evconnlistener *serv, int sock, struct sockaddr *sa,
-             int sa_len, void *arg)
-{
+static void ssl_acceptcb(struct evconnlistener *serv, int sock, struct sockaddr *sa,
+                         int sa_len, void *arg) {
     struct event_base *evbase;
     struct bufferevent *bev;
     SSL_CTX *server_ctx;
     SSL *client_ctx;
 
-    server_ctx = (SSL_CTX *)arg;
+    server_ctx = (SSL_CTX *) arg;
     client_ctx = SSL_new(server_ctx);
     evbase = evconnlistener_get_base(serv);
 
@@ -46,10 +42,8 @@ ssl_acceptcb(struct evconnlistener *serv, int sock, struct sockaddr *sa,
     bufferevent_setcb(bev, ssl_readcb, NULL, NULL, NULL);
 }
 
-static SSL_CTX *
-evssl_init(void)
-{
-    SSL_CTX  *server_ctx;
+static SSL_CTX *evssl_init(void) {
+    SSL_CTX *server_ctx;
 
     /* Initialize the OpenSSL library */
     SSL_load_error_strings();
@@ -60,13 +54,13 @@ evssl_init(void)
 
     server_ctx = SSL_CTX_new(SSLv23_server_method());
 
-    if (! SSL_CTX_use_certificate_chain_file(server_ctx, "cert") ||
-        ! SSL_CTX_use_PrivateKey_file(server_ctx, "pkey", SSL_FILETYPE_PEM)) {
+    if (!SSL_CTX_use_certificate_chain_file(server_ctx, "cert") ||
+        !SSL_CTX_use_PrivateKey_file(server_ctx, "pkey", SSL_FILETYPE_PEM)) {
         puts("Couldn't read 'pkey' or 'cert' file.  To generate a key\n"
-           "and self-signed certificate, run:\n"
-           "  openssl genrsa -out pkey 2048\n"
-           "  openssl req -new -key pkey -out cert.req\n"
-           "  openssl x509 -req -days 365 -in cert.req -signkey pkey -out cert");
+             "and self-signed certificate, run:\n"
+             "  openssl genrsa -out pkey 2048\n"
+             "  openssl req -new -key pkey -out cert.req\n"
+             "  openssl x509 -req -days 365 -in cert.req -signkey pkey -out cert");
         return NULL;
     }
     SSL_CTX_set_options(server_ctx, SSL_OP_NO_SSLv2);
@@ -74,9 +68,7 @@ evssl_init(void)
     return server_ctx;
 }
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     SSL_CTX *ctx;
     struct evconnlistener *listener;
     struct event_base *evbase;
@@ -92,9 +84,9 @@ main(int argc, char **argv)
         return 1;
     evbase = event_base_new();
     listener = evconnlistener_new_bind(
-                         evbase, ssl_acceptcb, (void *)ctx,
-                         LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, 1024,
-                         (struct sockaddr *)&sin, sizeof(sin));
+            evbase, ssl_acceptcb, (void *) ctx,
+            LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE, 1024,
+            (struct sockaddr *) &sin, sizeof(sin));
 
     event_base_loop(evbase, 0);
 
